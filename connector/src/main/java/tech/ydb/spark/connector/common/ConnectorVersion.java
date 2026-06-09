4705ad9f48c051c53a6f97382d88e34970baea76
Alexandr Gorshenin <alexandr268@ydb.tech>
2026-06-09 15:50:56 +0100

    Make version loading more safe

--- connector/src/main/java/tech/ydb/spark/connector/common/ConnectorVersion.java
+++ connector/src/main/java/tech/ydb/spark/connector/common/ConnectorVersion.java
@@ -1,18 +1,22 @@
 package tech.ydb.spark.connector.common;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import tech.ydb.core.utils.Version;
 
 public class ConnectorVersion {
+    private static final Logger logger = LoggerFactory.getLogger(ConnectorVersion.class);
+
     private final String version;
     private final String sdk;
 
-    private ConnectorVersion(String version) {
+    private ConnectorVersion(String version, String sdk) {
         this.version = version;
-        this.sdk = Version.getVersion().orElse("unknown");
+        this.sdk = sdk;
     }
 
     public String getConnectorVersion() {
@@ -20,7 +24,7 @@ public class ConnectorVersion {
     }
 
     public String getFullVersion() {
-        return "v" + version + "(based on SDK v" + sdk + ")";
+        return "v" + version + " (based on SDK v" + sdk + ")";
     }
 
     public static ConnectorVersion getInstance() {
@@ -33,13 +37,19 @@ public class ConnectorVersion {
 
         static {
             String version = "0.0-dev";
-            try (InputStream in = Version.class.getResourceAsStream(PROPERTIES_PATH)) {
-                Properties prop = new Properties();
-                prop.load(in);
-                version = prop.getProperty("version");
-            } catch (IOException e) { }
+            String sdk = "unknown";
+            try (InputStream in = ConnectorVersion.class.getResourceAsStream(PROPERTIES_PATH)) {
+                if (in != null) {
+                    Properties prop = new Properties();
+                    prop.load(in);
+                    version = prop.getProperty("version", version);
+                }
+                sdk = Version.getVersion().orElse("unknown");
+            } catch (Exception ex) {
+                logger.warn("cannot load ydb connector version", ex);
+            }
 
-            INSTANCE = new ConnectorVersion(version);
+            INSTANCE = new ConnectorVersion(version, sdk);
         }
     }
 }
